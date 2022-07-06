0fb735e55f256416b711778ba27e9852fab9fa02
morapelker <morapelker@gmail.com>
2022-07-06 13:55:00 +0300

    "v1.1.17"

--- TedoooProductGallery/Classes/ProductGallery/DI/ProductScreenImpl.swift
+++ TedoooProductGallery/Classes/ProductGallery/DI/ProductScreenImpl.swift
@@ -48,10 +48,12 @@ public class ProductScreenImpl: ProductGalleryScreen {
         return restApi.requestRx(outputType: ProductNotificationInformation.self, request: HttpRequest(path: "products/link/\(linkId)", token: nil)).map { res -> Any? in
             let v = GalleryViewController.create(id: res.shopId, coverPhoto: res.coverPhoto, urls: res.items.map({$0.imageUrl}), owned: res.owner.id == currentId, shopOwner: ShopOwner(id: res.owner.id, username: res.owner.username, avatar: res.owner.avatar), imagesChanged: nil)
             vc.present(v, animated: false) {
-                v.scroll(to: res.index)
-                v.enlargeProductAtIndex(res.index)
-                let specificShopVc = self.specificShopProvider.instantiate(id: res.shopId)
-                vc.pushViewController(specificShopVc, animated: false)
+                DispatchQueue.main.async {
+                    v.scroll(to: res.index)
+                    v.enlargeProductAtIndex(res.index)
+                    let specificShopVc = self.specificShopProvider.instantiate(id: res.shopId)
+                    vc.pushViewController(specificShopVc, animated: false)
+                }
             }
             return nil
         }.mapError({ _ in .invalidLink}).eraseToAnyPublisher()
